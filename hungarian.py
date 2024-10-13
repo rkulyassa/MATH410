45466736dace99ecddc915658f07122016c89f3b
ryan <rkulyassa@gmail.com>
2024-10-13 16:37:42 -0400

    Initial commit

---
+++ hungarian.py
@@ -0,0 +1,8 @@
+from hungarian_algorithm import algorithm
+from data import SH
+
+if __name__ == '__main__':
+    optimal_matching = algorithm.find_matching(SH, matching_type='max', return_type='list')
+    print(optimal_matching)
+    score = sum([g[1] for g in optimal_matching])
+    print(score)
