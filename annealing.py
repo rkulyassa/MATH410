c0d28cf2c10eb0b3171349f1046919d4da3788bc
ryan <rkulyassa@gmail.com>
2024-10-13 16:45:05 -0400

    formatting

--- annealing.py
+++ annealing.py
@@ -2,11 +2,14 @@ import random
 import math
 from data import S, C
 
+
 def random_course(courses: dict) -> str:
     return random.choice(list(courses.keys()))
 
+
 def random_student(students: dict) -> str:
     return random.choice(list(students.keys()))
+
 
 def course_full(matching: dict, courses: dict, target_course: str) -> bool:
     capacity = courses[target_course]
@@ -18,6 +21,7 @@ def course_full(matching: dict, courses: dict, target_course: str) -> bool:
             return True
     return False
 
+
 def random_matching(students: dict, courses: dict) -> dict:
     matching = {}
     for student in students.keys():
@@ -26,6 +30,7 @@ def random_matching(students: dict, courses: dict) -> dict:
             course = random_course(courses)
         matching[student] = course
     return matching
+
 
 def total_score(students: dict, matching: dict):
     total_score = 0
@@ -37,24 +42,29 @@ def total_score(students: dict, matching: dict):
         total_score += score
     return total_score
 
+
 def random_swap(matching: dict, students: dict, courses: dict):
     new_matching = matching.copy()
     student = random_student(students)
-    new_course = random_course(courses) # restrict to just courses that the student has?
+    new_course = random_course(
+        courses
+    )  # restrict to just courses that the student has?
     while course_full(matching, courses, new_course) or new_course == matching[student]:
         new_course = random_course(courses)
     new_matching[student] = new_course
     return new_matching
 
+
 def simulated_annealing(
-        students: dict,
-        courses: dict,
-        initial_matching: dict = None,
-        temperature: float = 1000,
-        cooling_rate: float = 0.99,
-        max_iterations: int = 1000,
-        logging: bool = False) -> dict:
-    
+    students: dict,
+    courses: dict,
+    initial_matching: dict = None,
+    temperature: float = 1000,
+    cooling_rate: float = 0.99,
+    max_iterations: int = 1000,
+    logging: bool = False,
+) -> dict:
+
     if not initial_matching:
         initial_matching = random_matching(students, courses)
 
@@ -62,7 +72,15 @@ def simulated_annealing(
     current_score = total_score(students, current_matching)
 
     for i in range(max_iterations):
-        if logging: print('Iteration', str(i).ljust(2), '| Score', str(current_score).ljust(2), '| Temperature', str(temperature))
+        if logging:
+            print(
+                "Iteration",
+                str(i).ljust(2),
+                "| Score",
+                str(current_score).ljust(2),
+                "| Temperature",
+                str(temperature),
+            )
         new_matching = random_swap(current_matching, students, courses)
         new_score = total_score(students, new_matching)
 
@@ -75,12 +93,13 @@ def simulated_annealing(
             if random.random() < acceptance_probability:
                 current_matching = new_matching
                 current_score = new_score
-        
+
         temperature *= cooling_rate
-    
+
     return current_matching
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     result = simulated_annealing(S, C)
     print(result)
     score = total_score(S, result)
