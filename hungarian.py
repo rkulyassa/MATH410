5c701513de82456b52cd7ca68832aaf938cbf2e8
Ryan Kulyassa <rkulyassa@gmail.com>
2025-03-14 23:27:30 -0400

    add munkres + HA to run on data

--- hungarian.py
+++ hungarian.py
@@ -1,9 +1,30 @@
+import csv
+
 from hungarian_algorithm import algorithm
-from data.data import SH
 
 if __name__ == "__main__":
+    with open("data/FA22.csv") as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+    
+    G = {}
+
+    class_names = rows[0][1:]
+
+    for row in rows[1:]:
+        timestamp = row[0]
+        G[timestamp] = {}
+        for i, v in enumerate(row[1:]):
+            if v.strip():
+                preference = int(v.strip())
+                if preference:
+                    G[timestamp][class_names[i]] = int(preference)
+    
+    # print(G)
+
+
     optimal_matching = algorithm.find_matching(
-        SH, matching_type="max", return_type="list"
+        G, matching_type="max", return_type="list"
     )
     print(optimal_matching)
     score = sum([g[1] for g in optimal_matching])
---
+++ kuhn-munkres.py
@@ -0,0 +1,42 @@
+import csv
+from munkres import Munkres, print_matrix, DISALLOWED
+
+if __name__ == "__main__":
+    with open("data/FA22.csv") as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+    
+    matrix = []
+    rows = [row[1:] for row in rows[1:]]
+    for row in rows:
+        r = []
+        for entry in row:
+            if entry.strip():
+                v = int(entry.strip())
+                r.append(-v + 5)
+            else:
+                r.append(DISALLOWED)
+        matrix.append(r)
+
+    i = 0
+    while len(matrix) > len(matrix[0]):
+        for row in matrix:
+            row.append(row[i])
+        i += 1
+        if i == 22:
+            i = 0
+    
+    # for m in matrix:
+    #     print(m)
+    # print(len(matrix))
+    # print(len(matrix[0]))
+
+    m = Munkres()
+    indexes = m.compute(matrix)
+    print_matrix(matrix, msg='Lowest cost through this matrix:')
+    total = 0
+    for row, column in indexes:
+        value = matrix[row][column]
+        total += value
+        print(f'({row}, {column}) -> {value}')
+    print(f'total cost: {total}')
